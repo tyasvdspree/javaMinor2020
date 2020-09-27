2bca4b2cec5ea2ef045fc11aef32180d0d641970
Tyas van der Spree <tyas@devoon.nl>
2020-09-27 14:16:34 +0200

    intermediate commit

--- src/main/java/assignments/javaminor/StringReversController.java
+++ src/main/java/assignments/javaminor/StringReversController.java
@@ -1,13 +1,25 @@
 package assignments.javaminor;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StringReversController {
+    @Resource(name = "requestModBean")
+    StringModBean requestModBean;
 
     @GetMapping("/stringrevers")
     public String handle(String input){
-        return new StringBuilder(input).reverse().toString();
+
+    }
+
+    @GetMapping("/stringCounter")
+    public String handle(String input){
+
     }
 }
